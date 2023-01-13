6aa825d0ab41c19e754e8259dcb577e766a6d50c
letrungtan <trungtan14@gmail.com>
2023-01-13 15:05:52 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -122,6 +122,8 @@ function handlePostback(sender_psid, received_postback) {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    } else if (payload === 'GET_STARTED') {
+        response = {"text": "Chào mừng bạn đến với dịch vụ đặt lịch khám HealthCare!"}
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
