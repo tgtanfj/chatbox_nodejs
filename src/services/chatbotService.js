86659d6bcc342167fe3c578426f3556269b864d7
letrungtan <trungtan14@gmail.com>
2023-01-13 23:54:32 +0700

    abc

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -27,10 +27,29 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
-let handleGetStarted = (sender_psid) => {
+let getUserName =  (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        request({
+            "uri": `curl -X GET https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            "method": "GET",
+        }, (err, res, body) => {
+            if (!err) {
+                body = JSON.parse(body)
+                let username = `${body.first_name} ${body.last_name}`
+                resolve(username)
+            } else {
+                console.error("Unable to send message:" + err);
+                reject(err)
+            }
+        });
+    })
+}
+
+let handleGetStarted = async (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            response = { "text": "Chào mừng bạn đến với dịch vụ đặt lịch khám HealthCare!" }
+            let username = await getUserName(sender_psid)
+            let response = { "text": `Chào mừng ${username} đến với dịch vụ đặt lịch khám HealthCare!` }
             await callSendAPI(sender_psid, response)
             resolve('OK')
         } catch (e) {
